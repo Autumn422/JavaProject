67e49d22b2cd1f5a1e9303fe9d845b7c07e3c453
Autumn422 <92914981+Autumn422@users.noreply.github.com>
2021-10-30 00:44:22 +0800

    Delete src directory

--- src/main/java/cn/cqu/edu/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package cn.cqu.edu.repository;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-import cn.cqu.edu.domain.User;
-@Repository
-public interface UserRepository extends MongoRepository<User, String> {
-}
